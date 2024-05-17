56afdf20e7cdf68399e8982a09c0fb88ceef7aba
JL3637 <jason36375667@gmail.com>
2024-05-18 03:00:18 +0800

    update

---
+++ agents/allin_player.py
@@ -0,0 +1,31 @@
+from game.players import BasePokerPlayer
+
+
+class AllInPlayer(
+    BasePokerPlayer
+):
+    #  we define the logic to make an action through this method. (so this method would be the core of your AI)
+    def declare_action(self, valid_actions, hole_card, round_state):
+        # valid_actions format => [raise_action_info, call_action_info, fold_action_info]
+        raise_action_info = valid_actions[2]
+        action, amount = raise_action_info["action"], raise_action_info["amount"]['max']
+        return action, amount
+
+    def receive_game_start_message(self, game_info):
+        pass
+
+    def receive_round_start_message(self, round_count, hole_card, seats):
+        pass
+
+    def receive_street_start_message(self, street, round_state):
+        pass
+
+    def receive_game_update_message(self, action, round_state):
+        pass
+
+    def receive_round_result_message(self, winners, hand_info, round_state):
+        pass
+
+
+def setup_ai():
+    return AllInPlayer()
--- start_game.py
+++ start_game.py
@@ -3,15 +3,19 @@ from game.game import setup_config, start_poker
 from agents.call_player import setup_ai as call_ai
 from agents.random_player import setup_ai as random_ai
 from agents.console_player import setup_ai as console_ai
+from agents.allin_player import setup_ai as allin_ai
 
-from baseline0 import setup_ai as baseline0_ai
+from baseline0 import setup_ai as baseline0_ai # type: ignore
 
 config = setup_config(max_round=20, initial_stack=1000, small_blind_amount=5)
 config.register_player(name="p1", algorithm=baseline0_ai())
-config.register_player(name="p2", algorithm=random_ai())
+# config.register_player(name="p2", algorithm=baseline0_ai())
+# config.register_player(name="p2", algorithm=random_ai())
+# config.register_player(name="p2", algorithm=call_ai())
+config.register_player(name="p2", algorithm=allin_ai())
 
 ## Play in interactive mode if uncomment
-#config.register_player(name="me", algorithm=console_ai())
+# config.register_player(name="me", algorithm=console_ai())
 game_result = start_poker(config, verbose=1)
 
 print(json.dumps(game_result, indent=4))
